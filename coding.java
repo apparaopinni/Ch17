61d9544c0633ed9542bf668ab683456f0f3a520e
apparaopinni <pinniapparao098@gmail.com>
2024-05-22 11:50:04 +0530

    added file

---
+++ coding.java
@@ -0,0 +1,21 @@
+class  Main extends demo
+{
+public static void main(String args[])
+   {
+        Main main=new Main();
+         main.m1();
+   }
+   public void m1()
+     {
+         System.out.println("Hello all good");
+     } 
+}
+class demo
+{
+    public void m1()
+     {
+         System.out.println("haii");
+     }
+}
+
+     
